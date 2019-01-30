d075861192b4848c39ef9109d9f5cdc6ca1b6ee5
tsoleary <tsoleary12@gmail.com>
2019-01-30 12:23:31 -0500

    separated functions

--- proteomics.R
+++ proteomics.R
@@ -1,22 +1,14 @@
 # Proteomic analysis: Data imported from Proteome Discoverer 2.2 ---------------
 
 library(tidyverse)
-source("~/Github/previs/proteomic_functions.R")
+
+#need to source in the functions or create an r package and install, library etc
+#source("~/Github/previs/proteomic_functions.R")
 
 setwd("C:/Users/PrevBeast/Documents/R/WT v KO mouse")
 data_raw <- read.csv("WT vs KO_pep.csv")
 
 # Normalization ----------------------------------------------------------------
-
-# Function applied to each peptide for a group of data with median as default
-as_group <- function (dat, col, FUN = median){
-  list <- NULL
-  for (i in 1:nrow(dat)){
-    temp <- FUN(as.numeric(dat[i, col], na.rm = TRUE))
-    list <- c(list, temp)
-  }
-  return(list)
-}
 
 ctrl_raw <- grep("Control", colnames(data_raw))
 data_raw$ctrl_raw_med <- as_group(data_raw, ctrl_raw)
@@ -61,20 +53,6 @@ data$ctrl_sd <- as_group(data, ctrl, FUN = sd)
 ratio <- data$group1_med / data$ctrl_med
 data$ratio <- ratio 
 
-# Function to count degrees of freedom in a group, defaults witout duplicates
-count_df <- function (dat, col, dup = 1){
-  list <- NULL
-  for (i in 1:nrow(dat)){
-    temp <- length(which(!is.na(dat[i, col])))
-    temp <- temp / dup
-    if(temp >= 1){
-      temp <- temp - 1
-    }
-    list <- c(list, temp)
-  }
-  return(list)
-}
-
 # Degrees of freedom
 data$group1_df <- count_df(data, group1, dup = 3)
 data$ctrl_df <- count_df(data, ctrl, dup = 3)
@@ -92,20 +70,6 @@ data_top <-
 
 # Protein Averages -------------------------------------------------------------
 
-# Relative abundance of each protein using top ionizers
-protein_group <- function (dat, groups, FUN = mean){
-  tab <- NULL
-  for (col in groups){
-    temp <- tapply(dat[, col],
-                   dat$Master.Protein.Accessions,
-                   FUN,
-                   na.rm = TRUE)
-    tab <- cbind(tab, temp)
-  }
-  colnames(tab) <- groups
-  return(tab)
-}
-
 group_names <- c("group1_med", "ctrl_med")
 
 protein <- protein_group(data_top, group_names) %>%
@@ -115,11 +79,6 @@ protein <- protein_group(data_top, group_names) %>%
 protein$Master.Protein.Accessions <-
   protein$Master.Protein.Accessions %>%
   as.character
-
-# Standard deviation of grouped relative abundance using top ionizers
-square_x_df <- function (dat, group_sd, group_df){
-  (dat[, group_sd])^2 * (dat[, group_df])
-}
 
 data_top$group1_sd_df <- square_x_df(data_top, "group1_sd", "group1_df")
 data_top$ctrl_sd_df <- square_x_df(data_top, "ctrl_sd", "ctrl_df")
@@ -136,18 +95,6 @@ protein <- cbind(protein, group1_pooled_sd, ctrl_pooled_sd)
 
 # Grouped relative protein abundance ratio
 protein$ratio <- protein_group(data, "ratio") %>% as.numeric
-
-# Taylor Expansion to get the sd of the ratio of two means
-ratio_sd <- function (dat, ratio, group1_sd, group1_med, ctrl_sd, ctrl_med){
-  ratio_sd_pep <- NULL
-  for (i in 1:nrow(dat)){
-    temp1 <- (dat[i, group1_sd] / dat[i, group1_med])^2
-    temp2 <- (dat[i, ctrl_sd] / dat[i, ctrl_med])^2
-    result <- dat[i, ratio] * sqrt(temp1 + temp2)
-    ratio_sd_pep <- c(ratio_sd_pep, result)
-  }
-  return(ratio_sd_pep)
-}
 
 # Standard deviation relative protein abundance ratio
 data$ratio_sd <- ratio_sd(data, "ratio", "group1_sd", "group1_med", 
@@ -180,19 +127,6 @@ colnames(ctrl_stack) <- c("Master.Protein.Accessions", "ctrl_log", "ctrl")
 
 stacked <- cbind(group1_stack, ctrl_stack[, 2:3])
 
-# t-test function
-pval_ttest <- function (dat, group, ctrl){
-  Master.Protein.Accessions <- NULL
-  p_value <- NULL
-  for (pro in unique(dat$Master.Protein.Accessions)){
-    temp <- filter(dat, dat$Master.Protein.Accessions == pro)
-    pval_temp <- t.test(temp[, group], temp[, ctrl])$p.value
-    Master.Protein.Accessions <- c(Master.Protein.Accessions, pro)
-    p_value <- c(p_value, pval_temp)
-  }
-  return(as.data.frame(cbind(Master.Protein.Accessions, p_value)))
-}
-
 p_vals <- pval_ttest(stacked, "group1_log", "ctrl_log")
 p_vals$Master.Protein.Accessions <-
   p_vals$Master.Protein.Accessions %>% 
@@ -208,54 +142,10 @@ protein <- full_join(protein, p_vals, by = "Master.Protein.Accessions")
 # Converting protein accession to gene symbol ----------------------------------
 gene_df <- read.csv('mouse_PD_accession_gene.csv')
 
-# Convert Master.Protein.Accession to gene symbol
-mpa_to_gene <- function (dat, gene_dat){
-  dat$gene <- dat$Master.Protein.Accessions
-  for (i in 1:nrow(dat)){
-    temp <- which(dat$gene[i] == gene_dat$Accession, TRUE)
-    if (length(temp) == 1){
-      dat$gene <- gsub(dat$gene[i], gene_dat$Gen[temp], dat$gene)
-    }
-  }
-  return(dat$gene)
-}
-
 data$gene <- mpa_to_gene(data, gene_df)
 protein$gene <- mpa_to_gene(protein, gene_df)
 
 # Remove outliers --------------------------------------------------------------
-
-# Remove outliers in peptide data
-rm_outliers <- function (dat, ratio){
-  
-  sd_ratio_temp_df <- protein_group(dat, ratio, FUN = sd) %>% 
-    as.data.frame %>% 
-    rownames_to_column("Master.Protein.Accessions") %>%
-    'colnames<-' (c("Master.Protein.Accessions", "sd_ratios"))
-  
-  pro_temp <- full_join(protein, sd_ratio_temp_df, 
-                        by = "Master.Protein.Accessions")
-  
-  pro_temp$max_ratio <- pro_temp$ratio + 2 * pro_temp$sd_ratio
-  pro_temp$min_ratio <- pro_temp$ratio - 2 * pro_temp$sd_ratio
- 
-  data_rm_out <- NULL
-  
-  for (pro in unique(dat$Master.Protein.Accessions)){
-    temp <- filter(dat, dat$Master.Protein.Accessions == pro)
-    
-    rm_high <- which(temp$ratio > pro_temp$max_ratio[which(
-      pro_temp$Master.Protein.Accessions == pro)])
-    
-    rm_low <- which(temp$ratio < pro_temp$min_ratio[which(
-      pro_temp$Master.Protein.Accessions == pro)]) 
-    
-    rm <- c(rm_high, rm_low)
-    temp_rm <- temp[-rm, ]
-    data_rm_out <- bind_rows(data_rm_out, temp_rm)
-  }
-  return(data_rm_out)
-}
 
 data_rm_out <- rm_outliers(data, "ratio")
 
--- proteomics_functions.R
+++ proteomics_functions.R
@@ -9,3 +9,110 @@ as_group <- function (dat, col, FUN = median){
   }
   return(list)
 }
+
+# Function to count degrees of freedom in a group, defaults without duplicates
+count_df <- function (dat, col, dup = 1){
+  list <- NULL
+  for (i in 1:nrow(dat)){
+    temp <- length(which(!is.na(dat[i, col])))
+    temp <- temp / dup
+    if(temp >= 1){
+      temp <- temp - 1
+    }
+    list <- c(list, temp)
+  }
+  return(list)
+}
+
+# Relative abundance of each protein using top ionizers
+protein_group <- function (dat, groups, FUN = mean){
+  tab <- NULL
+  for (col in groups){
+    temp <- tapply(dat[, col],
+                   dat$Master.Protein.Accessions,
+                   FUN,
+                   na.rm = TRUE)
+    tab <- cbind(tab, temp)
+  }
+  colnames(tab) <- groups
+  return(tab)
+}
+
+# Standard deviation of grouped relative abundance using top ionizers
+square_x_df <- function (dat, group_sd, group_df){
+  (dat[, group_sd])^2 * (dat[, group_df])
+}
+
+# Taylor Expansion to get the sd of the ratio of two means
+ratio_sd <- function (dat, ratio, group1_sd, group1_med, ctrl_sd, ctrl_med){
+  ratio_sd_pep <- NULL
+  for (i in 1:nrow(dat)){
+    temp1 <- (dat[i, group1_sd] / dat[i, group1_med])^2
+    temp2 <- (dat[i, ctrl_sd] / dat[i, ctrl_med])^2
+    result <- dat[i, ratio] * sqrt(temp1 + temp2)
+    ratio_sd_pep <- c(ratio_sd_pep, result)
+  }
+  return(ratio_sd_pep)
+}
+
+# t-test function
+pval_ttest <- function (dat, group, ctrl){
+  Master.Protein.Accessions <- NULL
+  p_value <- NULL
+  for (pro in unique(dat$Master.Protein.Accessions)){
+    temp <- filter(dat, dat$Master.Protein.Accessions == pro)
+    pval_temp <- t.test(temp[, group], temp[, ctrl])$p.value
+    Master.Protein.Accessions <- c(Master.Protein.Accessions, pro)
+    p_value <- c(p_value, pval_temp)
+  }
+  return(as.data.frame(cbind(Master.Protein.Accessions, p_value)))
+}
+
+
+# Convert Master.Protein.Accession to gene symbol
+mpa_to_gene <- function (dat, gene_dat){
+  dat$gene <- dat$Master.Protein.Accessions
+  for (i in 1:nrow(dat)){
+    temp <- which(dat$gene[i] == gene_dat$Accession, TRUE)
+    if (length(temp) == 1){
+      dat$gene <- gsub(dat$gene[i], gene_dat$Gen[temp], dat$gene)
+    }
+  }
+  return(dat$gene)
+}
+
+# Remove outliers in peptide data
+rm_outliers <- function (dat, ratio){
+  
+  sd_ratio_temp_df <- protein_group(dat, ratio, FUN = sd) %>% 
+    as.data.frame %>% 
+    rownames_to_column("Master.Protein.Accessions") %>%
+    'colnames<-' (c("Master.Protein.Accessions", "sd_ratios"))
+  
+  pro_temp <- full_join(protein, sd_ratio_temp_df, 
+                        by = "Master.Protein.Accessions")
+  
+  pro_temp$max_ratio <- pro_temp$ratio + 2 * pro_temp$sd_ratio
+  pro_temp$min_ratio <- pro_temp$ratio - 2 * pro_temp$sd_ratio
+  
+  data_rm_out <- NULL
+  
+  for (pro in unique(dat$Master.Protein.Accessions)){
+    temp <- filter(dat, dat$Master.Protein.Accessions == pro)
+    
+    rm_high <- which(temp$ratio > pro_temp$max_ratio[which(
+      pro_temp$Master.Protein.Accessions == pro)])
+    
+    rm_low <- which(temp$ratio < pro_temp$min_ratio[which(
+      pro_temp$Master.Protein.Accessions == pro)]) 
+    
+    rm <- c(rm_high, rm_low)
+    temp_rm <- temp[-rm, ]
+    data_rm_out <- bind_rows(data_rm_out, temp_rm)
+  }
+  return(data_rm_out)
+}
+
+
+
+
