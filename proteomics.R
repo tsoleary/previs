4c8035ea705a743646ae8a73e0b6ec8ae15daf0e
tsoleary <tsoleary12@gmail.com>
2019-05-02 16:39:36 -0400

    adjusted to make max_pep <- 5

--- proteomics.R
+++ proteomics.R
@@ -28,7 +28,7 @@ by_group <- function (dat, col, FUN = median) {
 data_raw$ctrl_raw_med <- by_group(data_raw, ctrl_raw)
 
 # set the max number of peptides used in analysis
-max_pep <- 15
+max_pep <- 5
 
 data <-
   tbl_df(data_raw) %>%
@@ -144,42 +144,6 @@ rm_outliers <- function (dat, pro_df, ratio, mult = 2){
 
 
 data <- rm_outliers(data, pro_out, "ratio")
-
-# Trying to create a function that will remove peptides with high sd_ratio
-
-rm_outliers2 <- function (dat, pro_df, ratio, mult = 2){
-  
-  sd_ratio_temp_df <- by_protein(dat, ratio, FUN = sd) %>%
-    as.data.frame %>%
-    rownames_to_column("Master.Protein.Accessions") %>%
-    'colnames<-' (c("Master.Protein.Accessions", "sd_ratios"))
-  
-  pro_temp <- dplyr::full_join(pro_df, sd_ratio_temp_df,
-                               by = "Master.Protein.Accessions")
-  
-  pro_temp$max_ratio <- pro_temp$ratio + mult * pro_temp$sd_ratio
-  pro_temp$min_ratio <- pro_temp$ratio - mult * pro_temp$sd_ratio
-  
-  data_rm_out <- NULL
-  
-  for (pro in unique(dat$Master.Protein.Accessions)){
-    temp <- dplyr::filter(dat, dat$Master.Protein.Accessions == pro)
-    
-    rm_high <- which(temp$ratio > pro_temp$max_ratio[which(
-      pro_temp$Master.Protein.Accessions == pro)])
-    
-    rm <- c(rm_high)
-    if (length(rm) > 0){
-      temp_rm <- temp[-rm, ]
-      data_rm_out <- dplyr::bind_rows(data_rm_out, temp_rm)
-    } else {
-      data_rm_out <- dplyr::bind_rows(data_rm_out, temp)
-    }
-  }
-  return(data_rm_out)
-}
-
-data <- rm_outliers2(data, pro_out, "ratio_sd")
 
 # Data frame with only top few ionizing peptides -------------------------------
 
@@ -305,3 +269,5 @@ protein$gene <- mpa_to_gene(protein, gene_df)
 min_pep <- 5 
 protein$peptides <- table(data$Master.Protein.Accessions)
 protein <- filter(protein, protein$peptides >= min_pep)
+
+write.csv(protein, "meyer_HFpEF_top_5.csv")
