15393d653dd8bbe667d2228de4c8d96fd63ec3cc
tsoleary <tsoleary12@gmail.com>
2019-05-07 15:02:05 -0400

    tidy data!

---
+++ M_M_3_corrected_values.R
@@ -0,0 +1,21 @@
+# Take pinpoint M and M+3 values to calculate M corrected and M+3 corrected ----
+
+library(tidyverse)
+
+# Tidy the data ----------------------------------------------------------------
+
+getwd()
+setwd("C:/Users/PrevBeast/Documents/R/Helms")
+list.files()
+
+df <- read.csv("reprex.csv")
+
+samples <- c("DF_A_T0", "DF_A_T2", "DF_B_T0", "DF_B_T2", 
+             "L2_A_T0", "L2_A_T2", "L2_B_T0", "L2_B_T2")
+
+df2 <- df %>% 
+  gather(sample, abundance, samples, na.rm = TRUE)
+
+df3 <- separate(df2, "sample", c("sample", "duplicate", "time"), sep = "_")
+
+# 
