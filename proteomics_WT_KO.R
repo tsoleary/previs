f33ee56319234459c50023b4e5490bb51360d7a0
tsoleary <tsoleary12@gmail.com>
2019-05-09 15:42:14 -0400

    proteomics R scripts added for WT_KO and HCM data specfically, to normalize to histones and myosin. still need to correct the removing of outliers for multiple groups for HCM script.

--- proteomics_WT_KO.R
+++ proteomics_WT_KO.R
@@ -1,17 +1,10 @@
 # Proteomic analysis: Data imported from Proteome Discoverer 2.2 ---------------
-# Markus Meyer HFpEF samples
+# WT v MyBP-C KO Mouse samples
 
 library(tidyverse)
 
-#if proteomixr has been updated on github
-#library("devtools")
-#remove.packages("proteomixr") # remove old proteomixr
-#install_github("tsoleary/proteomixr") # to get latest version
-#library("proteomixr")
-
-#getwd()
-setwd("C:/Users/PrevBeast/Documents/R/Meyer")
-data_raw <- read.csv("meyer_all_peptides.csv")
+setwd("C:/Users/PrevBeast/Documents/R/WT v KO mouse")
+data_raw <- read.csv("WT vs KO all pep.csv")
 
 # Normalization ----------------------------------------------------------------
 
@@ -37,8 +30,8 @@ data <-
   top_n(n = max_pep, wt = ctrl_raw_med)
 
 # proteins used for normalization
-myosin <- "P12883; P13533"
-histones <- "B4DR52; P06899"
+myosin <- "B2RQQ1; Q91Z83"
+histones <- "B2RTM0"
 
 norm_pro <- histones
 
@@ -57,7 +50,7 @@ data <- cbind(data, norm_test)
 
 # Median, sd, & ratio of peptides ----------------------------------------------
 
-group1 <- grep("HFpEF_norm", colnames(data))
+group1 <- grep("Sample_norm", colnames(data))
 ctrl <- grep("Control_norm", colnames(data))
 
 data$group1_med <- by_group(data, group1)
@@ -202,7 +195,7 @@ ratio_sd <- function (dat, ratio, group1_sd, group1_med, ctrl_sd, ctrl_med){
 }
 
 data$ratio_sd <- ratio_sd(data, "ratio", "group1_sd", "group1_med", 
-                             "ctrl_sd", "ctrl_med")
+                          "ctrl_sd", "ctrl_med")
 
 # Standard deviation grouped relative protein abundance ratio
 data$ratio_df <- data$group1_df + data$ctrl_df
@@ -221,28 +214,23 @@ colnames(log_norm) <- paste(colnames(log_norm), sep = "_", "log")
 data <- cbind(data, log_norm)
 
 log_cols <- grep("log", colnames(data))
-group1_log_cols <- grep("HFpEF_norm_log", colnames(data))
+group1_log_cols <- grep("Sample_norm_log", colnames(data))
 ctrl_log_cols <- grep("Control_norm_log", colnames(data))
 
 # Problem if different number of samples be in each group ----------------------
 
-length(group1_log_cols)
-length(ctrl_log_cols)
+length(group1_log_cols) - length(ctrl_log_cols) == 0
 
-#add two cols to HFpEF samples
-data$dummy1_HFpEF_norm_log <- NA
-data$dummy2_HFpEF_norm_log <- NA
-
-group1_log_cols <- grep("HFpEF_norm_log", colnames(data))
+group1_log_cols <- grep("Sample_norm_log", colnames(data))
 ctrl_log_cols <- grep("Control_norm_log", colnames(data))
 
 stacked <- data.frame(data[, "Master.Protein.Accessions"], 
-                           stack(data[, group1_log_cols]),
-                           stack(data[, ctrl_log_cols]))
+                      stack(data[, group1_log_cols]),
+                      stack(data[, ctrl_log_cols]))
 
 
 colnames(stacked) <- c("Master.Protein.Accessions", "group1_log", "samp", 
-                        "ctrl_log", "ctrl")
+                       "ctrl_log", "ctrl")
 
 pval_ttest <- function (dat, group, ctrl, col = "Master.Protein.Accessions"){
   name <- NULL
@@ -263,12 +251,12 @@ colnames(p_vals)[1] <- "Master.Protein.Accessions"
 
 p_vals$Master.Protein.Accessions <-
   p_vals$Master.Protein.Accessions %>% 
-    as.character
+  as.character
 
 protein <- full_join(protein, p_vals, by = "Master.Protein.Accessions")
 
 # Converting protein accession to gene symbol ----------------------------------
-gene_df <- read.csv('meyer_protein_accession_gene_list.csv')
+gene_df <- read.csv("mouse_PD_accession_gene.csv")
 
 mpa_to_gene <- function (dat, gene_dat){
   dat$gene <- dat$Master.Protein.Accessions
@@ -289,11 +277,82 @@ min_pep <- 5
 protein$peptides <- table(data$Master.Protein.Accessions)
 protein <- filter(protein, protein$peptides >= min_pep)
 
-write.csv(protein, "meyer_HFpEF_hist_norm.csv")
+write.csv(protein, "WT_v_KO_hist_norm.csv")
 
-# Question about hydroxylated peptides in collagen increasing ------------------
+################################################################################
+## STOP ------------------------------------------------------------------------
 
-# Filter only peptides that are modified to be hydroxylated
+# Group by sub-cellular compartment --------------------------------------------
+
+sub_cell_comp <- read.csv("sub_cell_comp_mouse.csv")
+sub_cell_comp <- subset(sub_cell_comp, !duplicated(sub_cell_comp$Gene))
+
+# Group genes into subcellular compartments on data
+data$compartment <- gene_to_comp2(data, sub_cell_comp,
+                                  level = "compartment")
+
+# data$sub_compartment <- gene_to_comp(data, sub_cell_comp, 
+#                                      level = "sub_compartment")
+
+# There is some sort of bug in the gene_to_comp function that results in this:
+# > data$compartment[1]
+# [1] "MitochondriaMMitochondriaiMitochondriatMitochondriaoMitochondriac
+# MitochondriahMitochondriaoMitochondrianMitochondriadMitochondriarMitochondriai
+# MitochondriaaMitochondria"
+
+gene_to_comp2 <- function (dat, comp_dat, level = "compartment") {
+  dat$comp <- dat$gene
+  for (i in 1:nrow(dat)) {
+    temp <- which(dat$gene[i] == comp_dat$Gene, TRUE)
+    if (length(temp) == 1) {
+      dat$comp[i] <- gsub(dat$comp[i], comp_dat[temp, level], 
+                          dat$comp, ignore.case = TRUE)
+    }
+  }
+  return(dat$comp)
+}
+
+# test code below
+
+data$comp <- data$gene
+which(data$gene[1] == sub_cell_comp$Gene, TRUE)
+
+
+data$compartment <- gene_to_comp(data, sub_cell_comp,
+                                 level = "compartment")
 
 
 
+
+################################################################################
+
+# Condense unique compartments into only one
+compart_list <- as.character(unique(sub_cell_comp$compartment))
+
+comp_simp <- data$compartment
+list <- NULL
+temp <- NULL
+for (pro in compart_list) {
+  for (i in 1:nrow(data)){
+    if (str_detect(comp_simp[i], pro) == TRUE){
+      temp <- str_extract(comp_simp[i], pro)
+    } else {
+      comp_simp[i] <- comp_simp[i]
+    }
+  }
+}
+
+data$compartment <- comp_simp
+
+# Compartments ratio average
+weighted_ratio <- cbind(sapply(split(data, data$compartment), 
+                               function (x) {weighted.mean(x$ratio, x$group1_med)}))
+
+# Statistics for compartments and sub-compartments------------------------------
+
+stack2 <- data.frame(data[, "compartment"], stack(data[, group1_log_cols]),
+                     stack(data[, ctrl_log_cols]))
+colnames(stack2) <- c("compartment", "group1_log", "samp", "ctrl_log", "ctrl")
+
+p_val2 <- pval_ttest(stack2, "group1_log", "ctrl_log", col = "compartment")
+colnames(p_val2)[1] <- "compartment"
