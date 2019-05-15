140ff8977eb5dd2323c5852ff18dd8b86aa7c344
tsoleary <tsoleary12@gmail.com>
2019-05-15 17:04:28 -0400

    work day

--- M_M_3_corrected_values.R
+++ M_M_3_corrected_values.R
@@ -1,4 +1,4 @@
-# Take pinpoint M and M+3 values to calculate M corrected and M+3 corrected ----
+# Take pinpoint isotopes plot and correct for synthesis and decay calc ---------
 
 library(tidyverse)
 library(plotly)
@@ -7,40 +7,31 @@ library(plotly)
 
 setwd("C:/Users/PrevBeast/Documents/R/Helms")
 
-df_messy <- read.csv("All_isotopes_organized.csv")
+df_messy <- read.csv("All_isotopes_organized_select_reprex.csv")
 
 samples <- colnames(df_messy)[4:length(colnames(df_messy))]
 
-df <- df_messy %>% 
+df_tidy <- df_messy %>% 
   gather(sample, abundance, samples) %>%
   separate("sample", c("group", "duplicate", "hrs"), sep = "_")
 
-df$hrs <- as.numeric(df$hrs)
+df_tidy$hrs <- as.numeric(df_tidy$hrs)
 
 # average together the duplicate lines
-df_avg <- df %>%
+df_avg <- df_tidy %>%
   group_by(protein, isotope, peptide, group, hrs) %>%
   summarize(abundance = mean(abundance))
 
 # spreads out to columns by isotope: M_0, M_3, & Sum
-df_spread <- as.data.frame(spread(df_avg, "isotope", "abundance"))
-
-# make a reprex small data frame
-df_isos <- select(df_spread, c("protein", "peptide", "group", "hrs", "M_0", 
-                               "M_3", "M_6", "Sum"))
-df_reprex <- df_isos %>% 
-  filter(df_isos$hrs == c(0, 12, 24, 36)) 
-
-df_reprex <- filter(df_reprex, df_reprex$protein == "MYBPC3")
-df_reprex <- filter(df_reprex, df_reprex$peptide == "LNFDLIQELSHEAR")
+df <- as.data.frame(spread(df_avg, "isotope", "abundance"))
 
 # plot by protein, peptide, and isotope ----------------------------------------
 
-pro <- as.character(unique(df_spread$protein))
+pro <- as.character(unique(df$protein))
 
 for (i in 1:length(pro)){
   
-  temp <- filter(df_spread, protein == pro[i])
+  temp <- filter(df, protein == pro[i])
   
   pep <- as.character(unique(temp$peptide))
   
@@ -93,7 +84,31 @@ for (i in 1:length(pro)){
 # Using plotly package ---------------------------------------------------------
 # http://www.rebeccabarter.com/blog/2017-04-20-interactive/
 
-# doing the M_0, M_3, Sum corrected math ---------------------------------------
+# histone normalization of all isotopes ----------------------------------------
+
+# average histones
+df_hist <- df %>%
+  group_by(protein, group, hrs) %>%
+  summarize(avg_pro = mean(Sum)) %>%
+  filter(protein == "Histones")
+
+df <- cbind(df, hist_avg = df_hist$avg_pro)
+
+# normailize all isotopes by histones 
+isotopes <- c("M_0", "M_1", "M_2", "M_3", "M_4", "M_5", "M_6", "M_7", "Sum")
+
+for (col in isotopes){
+  df[, paste(col, "hist_norm", sep = "_")] <- df[, col] / df$hist_avg
+}
+
+# count the number of leucines in a peptide ------------------------------------
+df$num_L <- str_count(df$peptide, pattern = "L")
+
+# M_0, M_3, M_6 correction -----------------------------------------------------
+
+# M_3 and M_6 correction
+
+# M_0 correction 
 
 
 
