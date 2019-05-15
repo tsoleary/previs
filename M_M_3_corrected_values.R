26154c3d65a2cd16933ae49d04c5ade902fb2f97
tsoleary <tsoleary12@gmail.com>
2019-05-15 14:22:33 -0400

    reprex?

--- M_M_3_corrected_values.R
+++ M_M_3_corrected_values.R
@@ -1,7 +1,5 @@
 # Take pinpoint M and M+3 values to calculate M corrected and M+3 corrected ----
 
-#require("plotly")
-
 library(tidyverse)
 library(plotly)
 
@@ -9,32 +7,34 @@ library(plotly)
 
 setwd("C:/Users/PrevBeast/Documents/R/Helms")
 
-df_messy <- read.csv("pinpoint_m_m_3_for_r.csv")
+df_messy <- read.csv("All_isotopes_organized.csv")
 
 samples <- colnames(df_messy)[4:length(colnames(df_messy))]
 
 df <- df_messy %>% 
   gather(sample, abundance, samples) %>%
-  separate("sample", c("Group", "duplicate", "hrs"), sep = "_")
+  separate("sample", c("group", "duplicate", "hrs"), sep = "_")
 
-# Correct M_0 and M_3 ----------------------------------------------------------
+df$hrs <- as.numeric(df$hrs)
 
+# average together the duplicate lines
 df_avg <- df %>%
-  group_by(protein, type, peptide, sample, hrs) %>%
+  group_by(protein, isotope, peptide, group, hrs) %>%
   summarize(abundance = mean(abundance))
-  
-df_avg$hrs <- as.numeric(df_avg$hrs)
 
-# spreads out by "type" M_0, M_3, Sum
+# spreads out to columns by isotope: M_0, M_3, & Sum
+df_spread <- as.data.frame(spread(df_avg, "isotope", "abundance"))
 
-df_spread <- as.data.frame(spread(df_avg, "type", "abundance"))
+# make a reprex small data frame
+df_isos <- select(df_spread, c("protein", "peptide", "group", "hrs", "M_0", 
+                               "M_3", "M_6", "Sum"))
+df_reprex <- df_isos %>% 
+  filter(df_isos$hrs == c(0, 12, 24, 36)) 
 
-# creates different data frames for each one.
-# df_sum <- filter(df_avg, type == "Sum")
-# df_m0 <- filter(df_avg, type == "M_0")
-# df_m3 <- filter(df_avg, type == "M_3")
+df_reprex <- filter(df_reprex, df_reprex$protein == "MYBPC3")
+df_reprex <- filter(df_reprex, df_reprex$peptide == "LNFDLIQELSHEAR")
 
-# trying to make a bunch of plots at once with a loop
+# plot by protein, peptide, and isotope ----------------------------------------
 
 pro <- as.character(unique(df_spread$protein))
 
@@ -68,26 +68,23 @@ for (i in 1:length(pro)){
   }
 }
 
-# so now a few things
-
-# 2. figure out how to fit a one phase decay to the M-0 values -- probably need 
+# figure out how to fit a one phase decay to the M-0 values -- probably need 
 # to do the corrected M-0 values
-# 3. maybe figure out how to put all three M_0, M_3, and Sum in the same sort of 
+# maybe figure out how to put all three M_0, M_3, and Sum in the same sort of 
 # space
-# 4. plotly interactive too
-# 5. make the rest of it nice
-# 
+# plotly interactive too
+# make the rest of it nice
 
 # working on aesthetics
-ggplot(data = temp_pep) +
-  geom_point(mapping = aes(x = hrs,
-                           y = as.numeric(temp_pep[, colnames(temp_pep)[7]]),
-                           fill = sample, alpha = 0.5),
-             shape = 21, size = 3, stroke = 2, alpha = 0.9) +
-  labs(title = paste(pro[5], pep_x, sep = " -- "),
-       subtitle = colnames(temp_pep)[7],
-       y = "Abundance", x = "Time (hrs)") +
-  theme_classic()
+# ggplot(data = temp_pep) +
+#   geom_point(mapping = aes(x = hrs,
+#                            y = as.numeric(temp_pep[, colnames(temp_pep)[7]]),
+#                            fill = sample, alpha = 0.5),
+#              shape = 21, size = 3, stroke = 2, alpha = 0.9) +
+#   labs(title = paste(pro[5], pep_x, sep = " -- "),
+#        subtitle = colnames(temp_pep)[7],
+#        y = "Abundance", x = "Time (hrs)") +
+#   theme_classic()
 
 # ggsave("test.pdf", dpi = 320) could save the specific files in a .pdf using
 # the paste0() as the argument of the ggsave() in the for loop.
@@ -95,7 +92,6 @@ ggplot(data = temp_pep) +
 
 # Using plotly package ---------------------------------------------------------
 # http://www.rebeccabarter.com/blog/2017-04-20-interactive/
-
 
 # doing the M_0, M_3, Sum corrected math ---------------------------------------
 
