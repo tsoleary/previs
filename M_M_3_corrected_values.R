2d1483e47e5202c0f8ecfea83ee10dae43a3541f
tsoleary <tsoleary12@gmail.com>
2019-05-15 10:47:29 -0400

    save .pdf using ggsave and dpi = to get a nice resolution image

--- M_M_3_corrected_values.R
+++ M_M_3_corrected_values.R
@@ -15,7 +15,7 @@ samples <- colnames(df_messy)[4:length(colnames(df_messy))]
 
 df <- df_messy %>% 
   gather(sample, abundance, samples) %>%
-  separate("sample", c("sample", "duplicate", "hrs"), sep = "_")
+  separate("sample", c("Group", "duplicate", "hrs"), sep = "_")
 
 # Correct M_0 and M_3 ----------------------------------------------------------
 
@@ -57,10 +57,12 @@ for (i in 1:length(pro)){
       g1 <- ggplot(data = temp_pep) +
               geom_point(mapping = aes(x = hrs, 
                                        y = as.numeric(temp_pep[, colnames(temp_pep)[j]]), 
-                                       color = sample), size = 3) + 
+                                       color = Group), 
+                         size = 3, alpha = 0.8) + 
               labs(title = paste(pro[i], pep_x, sep = " -- "), 
-                   subtitle = colnames(temp_pep)[j]) +
-              ylab("abundance")
+                   subtitle = colnames(temp_pep)[j],
+                   y = "Abundance", x = "Time (hrs)") +
+              theme_classic()
       plot(g1)
     }
   }
@@ -68,7 +70,6 @@ for (i in 1:length(pro)){
 
 # so now a few things
 
-# 1. work on the aesthetics of the graph (rounder points?)
 # 2. figure out how to fit a one phase decay to the M-0 values -- probably need 
 # to do the corrected M-0 values
 # 3. maybe figure out how to put all three M_0, M_3, and Sum in the same sort of 
@@ -77,12 +78,27 @@ for (i in 1:length(pro)){
 # 5. make the rest of it nice
 # 
 
+# working on aesthetics
+ggplot(data = temp_pep) +
+  geom_point(mapping = aes(x = hrs,
+                           y = as.numeric(temp_pep[, colnames(temp_pep)[7]]),
+                           fill = sample, alpha = 0.5),
+             shape = 21, size = 3, stroke = 2, alpha = 0.9) +
+  labs(title = paste(pro[5], pep_x, sep = " -- "),
+       subtitle = colnames(temp_pep)[7],
+       y = "Abundance", x = "Time (hrs)") +
+  theme_classic()
+
+# ggsave("test.pdf", dpi = 320) could save the specific files in a .pdf using
+# the paste0() as the argument of the ggsave() in the for loop.
+
+
 # Using plotly package ---------------------------------------------------------
 # http://www.rebeccabarter.com/blog/2017-04-20-interactive/
 
-ggplotly(graph, tooltip = c("protein"))
 
-pros <- as.character(unique(df_sum$protein))
+# doing the M_0, M_3, Sum corrected math ---------------------------------------
+
 
 
 
