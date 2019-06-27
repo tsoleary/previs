d0bf79ed87466e1d41898ca7d6e01b230951e081
tsoleary <tsoleary12@gmail.com>
2019-06-27 14:13:54 -0400

    taylor proteomics added

---
+++ taylor.R
@@ -0,0 +1,84 @@
+# Proteomic analysis: Data imported from Proteome Discoverer 2.2 ---------------
+
+library(tidyverse)
+
+# source the functions in the proteomics_functions.R script!
+
+setwd("C:/Users/PrevBeast/Documents/R/Taylor")
+data_raw <- 
+  read.csv("taylor_drosophila_all_peptides.csv")
+
+# Normalization ----------------------------------------------------------------
+
+ctrl_raw <- grep("F", colnames(data_raw))
+
+data_raw$ctrl_raw_med <- by_group(data_raw, ctrl_raw)
+
+# # set the max number of peptides used in analysis
+# max_pep <- 15
+# 
+# data <-
+#   tbl_df(data_raw) %>%
+#   group_by(Master.Protein.Accessions) %>%
+#   top_n(n = max_pep, wt = ctrl_raw_med)
+
+
+# # proteins used for normalization
+# histones <- "B2RTM0"
+# 
+# norm_pro <- histones
+
+# norm_pep <- subset(data, data$Master.Protein.Accessions == norm_pro)
+# numeric_cols <- which(sapply(norm_pep, is.numeric) == TRUE)
+# raw_abun <- numeric_cols[-length(numeric_cols)]
+# norm_value <- sapply(norm_pep[, raw_abun], mean, na.rm = TRUE)
+# 
+# raw_abun_mat <- as.matrix(data[, raw_abun])
+# 
+# norm_abun <- t(t(raw_abun_mat)/norm_value)
+# colnames(norm_abun) <- paste(colnames(norm_abun), sep = "_", "norm")
+# norm_test <- as.data.frame(norm_abun)
+# data <- as_tibble(data)
+# data <- cbind(data, norm_test)
+
+
+# # sum of every column
+# norm_value <- colSums(data_raw[, ctrl_raw], na.rm = TRUE)
+# 
+# raw_abun_mat <- as.matrix(data_raw[, ctrl_raw])
+# 
+# norm_abun <- t(t(raw_abun_mat)/norm_value)
+# colnames(norm_abun) <- paste(colnames(norm_abun), sep = "_", "norm")
+# norm_test <- as.data.frame(norm_abun)
+# data_raw <- as_tibble(data_raw)
+# data <- cbind(data_raw, norm_test)
+
+
+# Data frame with only top few ionizing peptides -------------------------------
+
+pep_top <- 3
+data_top <-
+  tbl_df(data_raw) %>%
+  group_by(Master.Protein.Accessions) %>%
+  top_n(n = pep_top, wt = ctrl_raw_med) %>%
+  as.data.frame
+
+# Protein Averages -------------------------------------------------------------
+
+group_names <- colnames(data_top)[grep("F", colnames(data_top))]
+
+protein <- by_protein(data_top, group_names) %>%
+  as.data.frame %>%
+  rownames_to_column("Master.Protein.Accessions")
+
+# Converting protein accession to gene symbol ----------------------------------
+gene_df <- read.csv('taylor_drosophila_gene_names.csv')
+
+protein$gene <- mpa_to_gene(protein, gene_df)
+
+# Minimum number of peptides for each protein group ----------------------------
+# min_pep <- 3 
+protein$peptides <- table(data_raw$Master.Protein.Accessions)
+# protein <- filter(protein, protein$peptides >= min_pep)
+
+write.csv(protein, "taylor_drosophila_thick_filament_r.csv")
