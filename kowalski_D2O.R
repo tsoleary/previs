a7542355eec3a1d1191bd1639d8f984502978bbb
tsoleary <tsoleary12@gmail.com>
2019-07-01 13:31:55 -0400

    created function to plot the non-linear curve fit of the corrected ratio

--- kowalski_D2O.R
+++ kowalski_D2O.R
@@ -1,6 +1,7 @@
 # Take pinpoint isotopes plot and correct for synthesis and decay calc ---------
 
 library(tidyverse)
+library(broom)
 
 # source the functions in the proteomics_functions.R script!
 
@@ -16,7 +17,7 @@ df_tidy <- df_messy %>%
   gather(sample, abundance, samples) %>%
   separate("sample", c("sex", "mouse", "leg", "week"), sep = "_")
 
-df_tidy$week <- as.numeric(df_tidy$week)
+df_tidy$week <- as.numeric(df_tidy$week) - 8
 
 # # average together the mice legs in the same week lines for each peptide
 # df <- df_tidy %>%
@@ -52,14 +53,8 @@ for (i in 1:nrow(df_avg)){
                                    df_avg$peptide == df_avg$peptide[i] &
                                    df_avg$sex == df_avg$sex[i] & 
                                    df_avg$leg == df_avg$leg[i] & 
-                                   df_avg$week == 8)]
-  
-  which(df_avg$protein == df_avg$protein[i] & 
-        df_avg$peptide == df_avg$peptide[i] &
-        df_avg$sex == df_avg$sex[i] & 
-        df_avg$leg == df_avg$leg[i] & 
-        df_avg$week == 8)
-  
+                                   df_avg$week == 0)]
+
   temp <- (df_avg$M_1[i] - (t0_ratio * df_avg$M_0[i])) / 
     (df_avg$M_0[i] + (df_avg$M_1[i] - (t0_ratio * df_avg$M_0[i])))
   
@@ -67,5 +62,60 @@ for (i in 1:nrow(df_avg)){
 
   }
 
-df_avg$ratio_cor <- ratios
+df_avg$ratio <- ratios
 
+# plot and run a non-linear regression
+
+# plot_iso_ratios function
+plot_iso_r <- function(dat, g_title, FUN = geom_jitter){
+  
+  g <- ggplot(dat, aes(x = week, y = ratio)) +
+    FUN(mapping = aes(x = week, y = ratio, fill = leg), 
+        alpha = 0.5, size = 3, pch = 21,  color = "black", width = 0.05) +
+    geom_smooth(method = "nls", 
+                method.args = list(formula = y ~ yf * x / (tf + x),
+                                   start = list(tf = 3, yf = .6)),
+                data = df_g,
+                se = FALSE,
+                aes(color = leg), show.legend = FALSE) +
+    labs(title = g_title, x = "Week", y = "M0/(M0+M1)\nCorrected", 
+         fill = "Leg") +
+    expand_limits(x = 0, y = 0) +
+    theme_classic()
+  
+  return(g)
+  
+}
+
+
+ggplot(df_g)
+
+
+df_g <- filter(df_avg, protein == "Myh4" & peptide == "LQDAEEHVEAVNSK")
+
+
+ggplot(df_g, aes(week, ratio)) +
+  geom_point() +   
+  geom_smooth(method = "nls", 
+              method.args = list(formula = y ~ yf * x / (tf + x),
+                                 start = list(tf = 3, yf = .6)),
+              data = df_g,
+              se = FALSE,
+              aes(color = leg))
+
+
+
+plot_iso_r(df_g, "WORK!")
+
+
+
+fit <- nls(ratio_cor ~ SSasymp(week, yf, y0, log_alpha), data = df_g)
+
+
+df_g %>% 
+  group_by(protein, peptide, leg) %>% 
+  do(fit = nls(ratio_cor ~ SSasymp(week, yf, y0, log_alpha), data = .)) %>% 
+  tidy(fit) %>% 
+  select(sensor, term, estimate) %>% 
+  spread(term, estimate) %>% 
+  mutate(alpha = exp(log_alpha))
