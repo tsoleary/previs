0f76234cbde21913f5306e7b516529d34108667d
nbwood <nbwood94@gmail.com>
2020-06-04 16:04:25 -0400

    Did not end up using this much so far, but it's some nice, fairly standard stuff.

---
+++ Zebrafish.R
@@ -0,0 +1,85 @@
+# Proteomic analysis: Data imported from Proteome Discoverer 2.2 ---------------
+# Yengo
+
+library(tidyverse)
+source("C:/Users/PrevBeast/Documents/GitHub/Previs/proteomics_functions.R")
+
+setwd("C:/Users/PrevBeast/Documents/R/Zebrafish 1219")
+data_raw <- 
+  read.csv("Zebrafish one larva signal tests Jan 2020 no TSO all peptides.csv")
+
+# Normalization ----------------------------------------------------------------
+
+ctrl_raw <- grep("F", colnames(data_raw))
+
+data_raw$ctrl_raw_med <- by_group(data_raw, ctrl_raw)
+
+# set the max number of peptides used in analysis
+max_pep <- 15
+
+data <-
+  tbl_df(data_raw) %>%
+  group_by(Master.Protein.Accessions) %>%
+  top_n(n = max_pep, wt = ctrl_raw_med)
+
+# # proteins used for normalization
+# histones <- "B2RTM0"
+# 
+# norm_pro <- histones
+# 
+# norm_pep <- subset(data, data$Master.Protein.Accessions == norm_pro)
+# numeric_cols <- which(sapply(norm_pep, is.numeric) == TRUE)
+# raw_abun <- numeric_cols[-length(numeric_cols)]
+# norm_value <- sapply(norm_pep[, raw_abun], mean, na.rm = TRUE)
+# 
+# raw_abun_mat <- as.matrix(data[, raw_abun])
+# 
+# norm_abun <- t(t(raw_abun_mat)/norm_value)
+# colnames(norm_abun) <- paste(colnames(norm_abun), sep = "_", "norm")
+# norm_test <- as.data.frame(norm_abun)
+# data <- as_tibble(data)
+# data <- cbind(data, norm_test)
+
+# # Median, sd, & ratio of peptides ----------------------------------------------
+# 
+# group1 <- grep("infected_norm", colnames(data))
+# group2 <- grep("HMM_norm", colnames(data))
+# ctrl <- grep("Control_norm", colnames(data))
+# 
+# data$group1_med <- by_group(data, group1)
+# data$group2_med <- by_group(data, group2)
+# data$ctrl_med <- by_group(data, ctrl)
+
+# Data frame with only top few ionizing peptides -------------------------------
+
+pep_top <- 5
+data_top <-
+  tbl_df(data) %>%
+  group_by(Master.Protein.Accessions) %>%
+  top_n(n = pep_top, wt = ctrl_raw_med) %>%
+  as.data.frame
+
+# # Protein Averages -------------------------------------------------------------
+# 
+# group_names <- colnames(data)[grep("norm", colnames(data))]
+# 
+# protein <- by_protein(data_top, group_names) %>%
+#   as.data.frame %>%
+#   rownames_to_column("Master.Protein.Accessions")
+# 
+# protein$Master.Protein.Accessions <-
+#   protein$Master.Protein.Accessions %>%
+#   as.character
+# 
+# # Converting protein accession to gene symbol ----------------------------------
+# gene_df <- read.csv('Zebrafish one larva signal tests Jan 2020 no TSO Gene list.csv')
+# 
+# data$gene <- mpa_to_gene(data, gene_df)
+# protein$gene <- mpa_to_gene(protein, gene_df)
+
+# Minimum number of peptides for each protein group ----------------------------
+min_pep <- 5 
+protein$peptides <- table(data$Master.Protein.Accessions)
+protein <- filter(protein, protein$peptides >= min_pep)
+
+write.csv(data_top, "ZF singles Jan 2020 top 5 only.csv")
