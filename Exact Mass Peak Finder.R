81a836f513adef33939dbb044f12ed0864cb9256
nbwood <nbwood94@gmail.com>
2020-07-23 13:11:47 -0400

    Cleaned up and explained better. Changed name of find_envelopes function to find_envelope.

--- Exact Mass Peak Finder.R
+++ Exact Mass Peak Finder.R
@@ -2,19 +2,36 @@ library(tidyverse)
 setwd("C:\\Users\\PrevBeast\\Documents\\R\\Previs\\D3L mouse study\\Ttn Peptides\\VLDTPG")
 
 ## In case you're on your second or third peptide..
-rm(envelope, envelopes, maxima)
-## Import clipboard of Exact Masses from chosen scan(s) in CSV format
-## (Code below omits header and sets appropriate colnames/)
+rm(envelope, envelopes, maxima, maxtemp, compiled_maxima)
 
-# pepcsv_list <- list.files()[grep("ALQEAH", list.files())]
+## Pep: Peptide identifier as used in source filenames
+pep <- "VLDTPG"
+## mz: Peptide m/z value
+mz <- 650.37
+## z: Peptide charge-state
+z <- 3
+## Tis: Source-tissue as used in source filenames
+tis <- "Atrial"
+## NSamp: Number of samples. As used in thus far, each individual is denoted only
+## by sample index.
+NSamp <- 22
 
-for (i in c(1:22)) {
-maxima <- find_maxima("VLDTPG", "LV", i)
-sample <- paste("LV", i)
+## For-loop runs find_maxima to find all local maxima in each clipboard file.
+## After each file's list of maxima is found, find_envelope attempts to find
+## the isotopic envelope of interest given the maxima df, m/z value, and charge
+## of the peptide. 
+## Given the vastly different x-axis values between sample clipboards, I append
+## a bunch of NA values to the compiled_maxima df so all columns have the same
+## length. This and 'envelopes' are the exports of note. The former mainly
+## being useful for manual validation.
+
+for (i in c(1:NSamp)) {
+maxima <- find_maxima(pep, tis, i)
+sample <- paste(tis, i)
 maxtemp <- maxima[ , 1:2]
 colnames(maxtemp) <- paste(colnames(maxtemp), sample)
 
-envelope <- find_envelopes(maxima, 650.373, 3, rep = sample)
+envelope <- find_envelope(maxima, mz, z, rep = sample)
 
 if (i == 1) {
   envelopes <- envelope
@@ -33,85 +50,15 @@ if (i == 1) {
 }
 }
 
-write.csv(envelopes, "VLDTPG all LV auto.csv", row.names = FALSE)
-write.csv(compiled_maxima, "VLDTPG all LV maxima.csv", row.names = FALSE)
+## Exports two separate CSVs of 'envelopes' and 'compiled_maxima' respectively
 
-## Find series - Before using, manually pick a m/z value from 'maxima' to be
-## first ion in series. MAY NOT BE INDEX OF 1!! Depends highly on subset!
-## May modify for cases in which selected starting-m/z is not M+0.
-# m0 <- 936.8044
-# m0ind <- which.min(abs(maxima$`m/z` - m0))
-# m0 <- maxima$`m/z`[m0ind]
-# envelope <- maxima[m0ind,]
-# NAcount <- 0
-# 
-# for (i in 1:21) {
-#   if (i == 1) {
-#   tar <- m0 + 0.33 } else {
-#   tar <- tar + 0.33
-#   }
-#   maxima$iondiff <- maxima$`m/z` - tar
-#   mindiff <- which.min(abs(maxima$iondiff))
-#   if (abs(maxima$iondiff[mindiff]) < 0.02) {
-#   envelope <- full_join(envelope, maxima[mindiff, ])
-#   tar <- maxima$'m/z'[mindiff] } else {
-#   NAcount <- NAcount + 1
-#   if (NAcount > 1) {
-#     break
-#   }
-#   blank <- c(NA, NA, NA, NA, NA)
-#   names(blank) <- names(envelope)
-#   envelope <- bind_rows(envelope, blank)
-#   
-#   next
-#   }
-# }
-# 
-# rm(tar)
+write.csv(envelopes, "VLDTPG all Atrial auto.csv", row.names = FALSE)
+write.csv(compiled_maxima, "VLDTPG all Atrial maxima.csv", row.names = FALSE)
 
 
-
-
-
-
-## To do: Functionalize script. Inputs should be M0 mass, charge at the very least.
-## Inputs for desired number of isotopomers and maximum deltamass might be good.
-## In cases where unlabeled peaks disappear, could be useful to start from
-## M+X peak.
-
-#############################################
-
-# find_envelope <- function(data, mass, charge, deltamax = 0.02) {
-# m0ind <- which.min(abs(data$`m/z` - mass))
-# m0 <- data$`m/z`[m0ind]
-# envelope <- data[m0ind,]
-# zreciprocal <- 1/charge
-# NAcount <- 0
-# 
-# for (i in 1:21) {
-#   if (i == 1) {
-#     tar <- m0 + zreciprocal } else {
-#       tar <- tar + zreciprocal
-#     }
-#   data$iondiff <- data$`m/z` - tar
-#   mindiff <- which.min(abs(data$iondiff))
-#   if (abs(data$iondiff[mindiff]) < deltamax) {
-#     envelope <- full_join(envelope, data[mindiff, ])
-#     tar <- data$'m/z'[mindiff] } else {
-#       NAcount <- NAcount + 1
-#       if (NAcount > 1) {
-#         break
-#       }
-#       blank <- c(NA, NA, NA, NA, NA)
-#       names(blank) <- names(envelope)
-#       envelope <- bind_rows(envelope, blank)
-#       
-#       next
-#     }
-# }
-# return(envelope)
-# }
-# rm(tar)
+## Run these first! This is the quick-and-dirty heights script (as opposed to
+## using actual peak-areas, which would be more rigorous), so I'm hestitant
+## to put these in the actual proteomics_functions.R script.
 
 find_maxima <- function(peptide, tissue, rep) {
   
@@ -141,7 +88,7 @@ find_maxima <- function(peptide, tissue, rep) {
   return(maxima)
 }
 
-find_envelopes <- function(data, mass, charge, rep, deltamax = 0.02, NAcounter = TRUE) {
+find_envelope <- function(data, mass, charge, rep, deltamax = 0.02, NAcounter = TRUE) {
   m0ind <- which.min(abs(data$`m/z` - mass))
   m0 <- data$`m/z`[m0ind]
   envelope <- data[m0ind,]
