9d85a579e72fb70ca011ab7462f6e9062d9d2fc9
nbwood <nbwood94@gmail.com>
2020-12-09 16:57:12 -0500

    Exists

---
+++ Finch Proteins.R
@@ -0,0 +1,123 @@
+# Proteomic analysis: Data imported from Proteome Discoverer 2.2 ---------------
+
+library(tidyverse)
+source("C:/Users/PrevBeast/Documents/GitHub/Previs/proteomics_functions.R")
+
+setwd("C:/Users/PrevBeast/Documents/R/Elemans")
+data_raw <- 
+  read.csv("T guttata Iris FASTAs and Histones only All Peptides.csv")
+
+# Normalization ----------------------------------------------------------------
+
+ctrl_raw <- grep("F", colnames(data_raw))
+
+data_raw$ctrl_raw_med <- by_group(data_raw, ctrl_raw)
+
+data_raw <- filter(data_raw, (is.na(ctrl_raw_med) == FALSE))
+
+# set the max number of peptides used in analysis
+max_pep <- 5
+
+data <-
+  as_tibble(data_raw) %>%
+  group_by(Master.Protein.Accessions) %>%
+  top_n(n = max_pep, wt = ctrl_raw_med)
+
+
+# proteins used for normalization
+h4 <- "B5FXC8"
+
+
+# norm_pro <- titin
+norm_pro <- h4
+
+norm_pep <- subset(data, data$Master.Protein.Accessions == norm_pro)
+numeric_cols <- which(sapply(norm_pep, is.numeric) == TRUE)
+raw_abun <- numeric_cols[-length(numeric_cols)]
+norm_value <- sapply(norm_pep[, raw_abun], mean, na.rm = TRUE)
+
+raw_abun_mat <- as.matrix(data[, raw_abun])
+
+norm_abun <- t(t(raw_abun_mat)/norm_value)
+colnames(norm_abun) <- paste(colnames(norm_abun), sep = "_", "norm")
+norm_test <- as.data.frame(norm_abun)
+data <- as_tibble(data)
+data <- cbind(data, norm_test)
+
+
+# # sum of every column
+# norm_value <- colSums(data_raw[, ctrl_raw], na.rm = TRUE)
+# 
+# raw_abun_mat <- as.matrix(data[, ctrl_raw])
+# 
+# norm_abun <- t(t(raw_abun_mat)/norm_value)
+# colnames(norm_abun) <- paste(colnames(norm_abun), sep = "_", "norm")
+# norm_test <- as.data.frame(norm_abun)
+# data <- as_tibble(data)
+# data <- cbind(data, norm_test)
+
+# Data frame with only top few ionizing peptides -------------------------------
+
+# pep_top <- 3
+# data_top <-
+#   tbl_df(data) %>%
+#   group_by(Master.Protein.Accessions) %>%
+#   top_n(n = pep_top, wt = ctrl_raw_med) %>%
+#   as.data.frame
+
+# Protein Averages -------------------------------------------------------------
+
+group_names <- colnames(data)[grep("norm", colnames(data))]
+
+protein <- by_protein(data, group_names) %>%
+  as.data.frame %>%
+  rownames_to_column("Master.Protein.Accessions")
+
+# Converting protein accession to gene symbol ----------------------------------
+gene_df <- read.csv('T guttata Iris FASTAs and Histones only Gene List simple.csv')
+
+data$gene <- mpa_to_gene(data, gene_df)
+protein$gene <- mpa_to_gene(protein, gene_df)
+
+# Minimum number of peptides for each protein group ----------------------------
+min_pep <- 1
+protein$peptides <- table(data_raw$Master.Protein.Accessions)
+protein <- filter(protein, protein$peptides >= min_pep)
+
+# Sort by gene before export
+
+data <- arrange(data, gene)
+protein <- arrange(protein, gene)
+
+
+# watchlist <- read.csv("Uniprot and NCBI Identifiers Tgut Myosins and MYBP.csv")
+# for (i in watchlist$Identifier) {
+#   if (i == "") {
+#     next
+#   }
+#   if (i == watchlist$Identifier[1]) {
+#   specialprotein <- slice(protein, grep(i, protein$Master.Protein.Accessions)) 
+#   } else {
+#   specialprotein <- union(specialprotein, slice(protein, grep(i, protein$Master.Protein.Accessions))) 
+#   }
+#   
+# }
+# 
+# 
+# for (i in watchlist$Identifier) {
+#   if (i == "") {
+#     next
+#   }
+#   if (i == watchlist$Identifier[1]) {
+#     specialpeptide <- slice(data, grep(i, data$Master.Protein.Accessions))
+#   } else {
+#     specialpeptide <- union(specialpeptide, slice(data, grep(i, data$Master.Protein.Accessions)))
+#   }
+#   print(i)
+# }
+
+write.csv(protein, "110620 H4 norm proteins by Top3 no singlequote.csv")
+# write.csv(specialprotein, "H4 norm protein watchlist by Top3.csv")
+write.csv(data, "110620 H4 Norm all peptides Top5 simple gene.csv")
+# write.csv(specialpeptide, "H4 Norm watchlist peptides Top5.csv")
+
