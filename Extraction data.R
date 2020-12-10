fe4944afa4debeed413631c96fdcc20bd11b8758
nbwood <nbwood94@gmail.com>
2020-12-10 15:22:13 -0500

    Quick summary stats for extraction data. Might try to determine a better way to use tidyr::spread instead of separating variables manually and joining after. Did not seem immediately obvious.

--- Extraction data.R
+++ Extraction data.R
@@ -84,10 +84,79 @@ min_pep <- 2
 protein$peptides <- table(data_raw$Master.Protein.Accessions)
 protein <- filter(protein, protein$peptides >= min_pep)
 
+# Summary Sheet ----------------------------------------------------------------
+
+# Gather separate file abundances to single column, and separate filename into
+# several variables
+pbyfile <- gather(protein, contains("F"), key = "file", value = "abundance")
+septest <- separate(pbyfile, file, into = c("file", "frac","conc","salt","type"))
+
+# Assign replicate-number by PD file-identifier
+indlist <- read.csv("WT 150 200 250 Allreps 1209 identifiers.csv")
+septest$rep <- file_to_ind(septest, indlist)
+
+# Replace NA, NaN values with zeroes
+septest$abundance[is.na(septest$abundance)] = 0
+
+# Spread fraction variable into separate abundance columns "A", "B", "P", and 
+# join together while dropping nonessential variables
+A <- filter(septest, frac == "A") %>%
+  select(., c(1:3, 6, 10, 9)) %>%
+  rename(., A = abundance)
+B <- filter(septest, frac == "B") %>%
+  select(., c(1:3, 6, 10, 9)) %>%
+  rename(., B = abundance)
+P <- filter(septest, frac == "P") %>%
+  select(., c(1:3, 6, 10, 9)) %>%
+  rename(., P = abundance)
+
+jointest <- full_join(A, B) %>%
+  full_join(., P)
+
+# Calculate soluble and insoluble fraction within each conc., rep., and gene
+jointest <- mutate(jointest, total = A+B+P)
+jointest <- mutate(jointest, insoluble = P/total)
+jointest <- mutate(jointest, soluble = (A+B)/total)
+
+# Export values in current state (no averaging)
+write.csv(jointest, "extraction individual reps test.csv")
+
+# Compute summary stats within conc. and gene
+
+solsummary <- group_by(jointest, conc, gene) %>%
+  summarise(., solavg = mean(soluble, na.rm = TRUE), solsd = sd(soluble, na.rm = TRUE),
+            peptides = mean(peptides)) %>%
+  ungroup(.)
+
+
+# mean <- group_by(jointest, conc, gene) %>%
+#   summarise(., solavg = mean(soluble, na.rm = TRUE))
+# sd <- group_by(jointest, conc, gene) %>%
+#   summarise(., solsd = sd(soluble, na.rm = TRUE))
+# solsummary <- full_join(mean, sd) %>%
+#   ungroup(.)
+
+# Spread conc. variable into multiple mean columns
+
+Low <- filter(solsummary, conc == 150) %>%
+  select(-conc) %>%
+  rename(., avg150 = solavg, sd150 = solsd)
+Mid <- filter(solsummary, conc == 200) %>%
+  select(-conc) %>%
+  rename(., avg200 = solavg, sd200 = solsd)
+High <- filter(solsummary, conc == 250) %>%
+  select(-conc) %>%
+  rename(., avg250 = solavg, sd250 = solsd)
+
+Concsum <- full_join(Low, Mid) %>%
+  full_join(., High) %>%
+  mutate(., "200/150" = avg200/avg150, "250/150" = avg250/avg150)
+
 # Export -----------------------------------------------------------------------
 
 write.csv(protein, "Proteins.csv")
 # write.csv(specialprotein, "H4 norm protein watchlist by Top3.csv")
 write.csv(data, "Peptides.csv")
 # write.csv(specialpeptide, "H4 Norm watchlist peptides Top5.csv")
+write.csv(Concsum, "Full extraction summary 150 200 250 WT with peptide counts.csv")
 
